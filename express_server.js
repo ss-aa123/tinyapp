0cdddf9c8533272b97994e03fa6862e9a110dd09
ss-aa123 <neermas1@hotmail.com>
2022-12-04 23:19:40 -0500

    adding a POST route to remove urls

--- express_server.js
+++ express_server.js
@@ -55,10 +55,16 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id/delete", (req,res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+})
+
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id]
+  const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
+
 
 
 /* edge cases
