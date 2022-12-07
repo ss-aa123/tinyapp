399c32c0d49ffdc0e20bee2ac6de62198a8ab072
ss-aa123 <neermas1@hotmail.com>
2022-12-06 22:53:27 -0500

    created a function which returns URLS for only the current logged in user.This lets the user see their urls and edit or dlete them. if the user isn't logged in, they don't see any urls and if the wrong suer is logged in, they don't see anyone else's urls, but only their own. Also updated endpoints for security measures.

--- express_server.js
+++ express_server.js
@@ -27,10 +27,26 @@ function getUserByValue(emaill, database) {
   }
 }
 
+function urlsForUser(id, database) {
+  let usersUrls = {};
+  for (let id in database) {
+    if (database[id].userID === id) {
+      usersUrls[id] = database[id];
+    }
+  }
+  return usersUrls;
+};
+
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
 
 const users = {
@@ -45,6 +61,8 @@ const users = {
     password: "dishwasher-funk",
   },
 };
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -69,9 +87,14 @@ app.get("/hello", (req, res) => {
 
 //visit: http://localhost:8080/urls --> you see something on the page based on the template i guess
 app.get("/urls", (req, res) => {
+  if (!req.cookies["user_id"]) {
+    //need to return an HTML with a relevant error message if the user is not logged in. something like "please log in or register first to see urls"
+    res.redirect('/login')
+  }
   //pass user object to urls_index template
   let userID = req.cookies["user_id"]
-  const templateVars = { urls: urlDatabase, user: users[userID]  };
+  let userUrls = urlsForUser(userID, urlDatabase);
+  const templateVars = { urls: userUrls, user: users[userID]  };
   res.render("urls_index", templateVars);
 });
 
@@ -85,7 +108,7 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   if (!req.cookies["user_id"]) {
-    const message = 'You do not have permission to do that';
+    const message = 'You do not have permission to shorten URLS';
     res.status(403).send(message);
   }
   urlDatabase[req.params.id] = req.body.longURL 
@@ -94,26 +117,62 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
+
+  //if not logged in, url pages isnt accessible
+  //if logged in but url page doesnt belong to them, its not accessible to them
+  if (!urlDatabase[req.params.id]) {
+    const message = 'This url does not exist';
+    res.status(404).send(message);
+  } else if (!req.cookies["user_id"]) {
+    const message = 'Please log in or register first';
+    res.status(401).send(message);
+  } else if (!userUrls[id]) {
+    const message = 'You don\'t have permission to see this';
+    res.status(403).send(message);
+  }
   const id = req.params.id;
-  const templateVars = { id: id, longURL: urlDatabase[id], user: users[req.cookies["user_id"]]  };
+  const templateVars = { id: id, longURL: urlDatabase[id].longURL, user: users[req.cookies["user_id"]]  };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls/:id/delete", (req,res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
+  const user = req.cookies["user_id"];
+
+  if (!urlDatabase[req.params.id]) {
+    const message = 'This url does not exist';
+    res.status(404).send(message);
+  } else if (!req.cookies["user_id"]) {
+    const message = 'Please log in or register first';
+    res.status(401).send(message);
+  } else if (!userUrls[id]) {
+    const message = 'You don\'t have permission to see this';
+    res.status(403).send(message);
+  } else if (user && user === urlDatabase[id].userID) {
+    delete urlDatabase[req.params.id];
+    res.redirect("/urls");
+  }  
 })
 
 app.get("/u/:id", (req, res) => {
   if (!urlDatabase[req.params.id]) {
     const message = 'This url does not exist';
-    res.status(403).send(message);
+    res.status(404).send(message);
   }
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL); 
 });
 
 app.post("/urls/:id", (req,res) => {
+  if (!urlDatabase[req.params.id]) {
+    const message = 'This url does not exist';
+    res.status(404).send(message);
+  } else if (!req.cookies["user_id"]) {
+    const message = 'Please log in or register first';
+    res.status(401).send(message);
+  } else if (!userUrls[id]) {
+    const message = 'You don\'t have permission to see this';
+    res.status(403).send(message);
+  }
   urlDatabase[req.params.id] = req.body.updateLongURL
   const templateVars = { id: req.params.id, longURL: req.body.longURL  };
   res.redirect("/urls");
